33d116eba598739b8e8618c39fc952fac182272e
Berg <berg@vidahouse.com>
2020-03-17 19:46:34 +0800

    Unit1 Project Basics

--- Source/FPSGame/Public/FPSObjectiveActor.h
+++ Source/FPSGame/Public/FPSObjectiveActor.h
@@ -6,23 +6,36 @@
 #include "GameFramework/Actor.h"
 #include "FPSObjectiveActor.generated.h"
 
+class USphereComponent;
+
 UCLASS()
-class FPSGAME_API AFPSObjectiveActor : public AActor
+class FPSGAME_API AFPSObjectiveActor : public AActor 
 {
 	GENERATED_BODY()
 	
 public:	
 	// Sets default values for this actor's properties
-	AFPSObjectiveActor();
+	AFPSObjectiveActor(); 
 
 protected:
+	UPROPERTY(VisibleAnywhere,Category = "Components")
+	UStaticMeshComponent* MeshComp;
+
+	UPROPERTY(VisibleAnywhere,Category = "Components")
+	USphereComponent* SphereComp;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Effects")
+	UParticleSystem* PickupFX;
+	
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	void PlayEffects();
 
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	
+	virtual void NotifyActorBeginOverlap(AActor* OtherActor) override;
 	
 };
