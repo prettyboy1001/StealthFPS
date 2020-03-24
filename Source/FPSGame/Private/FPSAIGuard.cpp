0e447cf1a9bde30c4ecfd35778654ab1072aede1
Berg <berg@vidahouse.com>
2020-03-24 17:41:12 +0800

    028 Setup guard distraction

--- Source/FPSGame/Private/FPSAIGuard.cpp
+++ Source/FPSGame/Private/FPSAIGuard.cpp
@@ -21,7 +21,9 @@ AFPSAIGuard::AFPSAIGuard()
 void AFPSAIGuard::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	OriginalRotation = GetActorRotation();	
+
 }
 
 void AFPSAIGuard::OnPawnSeen(APawn* SeenPawn)
@@ -38,6 +40,24 @@ void AFPSAIGuard::OnPawnSeen(APawn* SeenPawn)
 void AFPSAIGuard::OnNoiseHeard(APawn* NoiseInstigator, const FVector& Location, float Volume)
 {
 	DrawDebugSphere(GetWorld(), Location, 32.0f, 12, FColor::Green, false, 10.0f);
+
+	FVector Direction = Location - GetActorLocation();
+	Direction.Normalize();
+
+	FRotator NewLookAt = FRotationMatrix::MakeFromX(Direction).Rotator();
+	NewLookAt.Pitch = 0.0f;
+	NewLookAt.Roll = 0.0f;
+
+	SetActorRotation(NewLookAt);
+
+	GetWorldTimerManager().ClearTimer(TimerHandle_ResetOrientation);
+	GetWorldTimerManager().SetTimer(TimerHandle_ResetOrientation, this, &AFPSAIGuard::ResetOrientation, 3.0f);
+	
+}
+
+void AFPSAIGuard::ResetOrientation()
+{
+	SetActorRotation(OriginalRotation);
 }
 
 // Called every frame
@@ -46,5 +66,4 @@ void AFPSAIGuard::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 }
-
 
--- Source/FPSGame/Public/FPSAIGuard.h
+++ Source/FPSGame/Public/FPSAIGuard.h
@@ -29,10 +29,19 @@ protected:
 
 	UFUNCTION()
 	void OnNoiseHeard(APawn* NoiseInstigator, const FVector& Location, float Volume);
-	
+
+	FRotator OriginalRotation;
+
+	void ResetOrientation();
+
+	FTimerHandle TimerHandle_ResetOrientation;
+
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 
 };
+
+
